11b97ddb0ebd2aec414d0ff42c2a31a3408a5b6f
Ihor Notevskyi <kolyvanoff@ukr.net>
2018-09-09 02:08:48 +0300

    Add changes

--- assignment_dump.sql
+++ assignment_dump.sql
@@ -33,7 +33,7 @@ CREATE TABLE `address` (
   PRIMARY KEY (`id`),
   KEY `address_country_id_fk` (`country_code_id`),
   CONSTRAINT `address_country_id_fk` FOREIGN KEY (`country_code_id`) REFERENCES `country` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `address` (
 
 LOCK TABLES `address` WRITE;
 /*!40000 ALTER TABLE `address` DISABLE KEYS */;
-INSERT INTO `address` VALUES (1,55,'00043400','Kirovograd','Bulgakova','57','423'),(2,56,'3212','Lviv','Ленина','14','5'),(3,45,'54353','Lyon','Peautie','47','56-A'),(4,13,'543534','Лондон','Кингстон','345 A','54'),(5,247,'23434','Марсель','Blaukers','14/56','654'),(7,99,'65476','Нэшвилл','Bingols','135/145 A','123'),(8,1,'54646','Чикаго','Знамения','54',''),(14,8,'9876','Брисбэн','Керченская','3',''),(15,16,'5345','Керч','Волгоградская','1','321'),(16,178,'03151','Бостон','Братская','42',''),(17,235,'645654','Paris','Уолл','43','1231'),(21,25,'05680','Milan','Брайтонская','13',''),(22,29,'00000','Турин','Barrero','78','312'),(23,79,'231245','Манчестер','Broncado','13 Г',''),(24,41,'656546','Запорожье','Станиславская','34','123B'),(25,220,'123','Вильнюс','Клаудская','43',''),(29,220,'543535','Стокгольм','Dentonskaya','76','134GH'),(30,220,'543547','Кливленд','Marrow','176',''),(31,220,'42342','Даллас','Sparrow','5','875R'),(32,220,'213123','Сент-Луис','Nionamy','15','324DF'),(33,220,'13434','Сан-Хосе','Kollingtons','73',''),(38,74,'543543','Ровно','Barritona','55','123'),(39,66,'95484','Lutsk','Posnyakovskaya','156/148 A','345-1');
+INSERT INTO `address` VALUES (1,55,'00043400','Kirovograd','Bulgakova','57','423'),(2,56,'3212','Lviv','Ленина','14','5'),(3,220,'54353','Lyon','Peautie','47','56-A'),(4,13,'543534','Лондон','Кингстон','345 A','54'),(5,247,'23434','Марсель','Blaukers','14/56','654'),(7,99,'65476','Нэшвилл','Bingols','135/145 A','123'),(8,1,'54646','Чикаго','Знамения','54',''),(14,8,'9876','Брисбэн','Керченская','3',''),(15,16,'5345','Керч','Волгоградская','1','321'),(16,178,'03151','Бостон','Братская','42',''),(17,235,'645654','Paris','Уолл','43','1231'),(21,25,'05680','Milan','Брайтонская','13',''),(22,29,'00000','Турин','Barrero','78','312'),(23,79,'231245','Манчестер','Broncado','13 Г',''),(24,41,'656546','Запорожье','Станиславская','34','123B'),(25,220,'123','Вильнюс','Клаудская','43',''),(29,220,'543535','Стокгольм','Dentonskaya','76','134GH'),(30,220,'543547','Кливленд','Marrow','176',''),(31,220,'42342','Даллас','Sparrow','5','875R'),(32,220,'213123','Сент-Луис','Nionamy','15','324DF'),(33,220,'13434','Сан-Хосе','Kollingtons','73',''),(38,74,'543543','Ровно','Barritona','55','123'),(39,66,'95484','Lutsk','Posnyakovskaya','156/148 A','345-1');
 /*!40000 ALTER TABLE `address` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +90,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_login_uindex` (`login`),
   UNIQUE KEY `users_email_uindex` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +99,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Mikky-Donk','$2y$13$.qH2G7LHkS02yw94iIKd5uo2GDktK5kqsZ.IDiQAH4.yUAk44EGZi','Mikelangello','Browns',2,'2018-09-05 01:56:37','mikes@test.com'),(2,'lordon','$2y$13$ON2aTHZEa78gJd4PnEbQTO32Iq4C8IqV.a2MrQ9XJZJwNPm0MQtHy','Джонни','Блэк',1,'2018-09-05 19:34:28','john@test.com'),(18,'Monk Donk','$2y$13$WvrPM0fHpmsJ4EyFRsLleOtcWnnkpYwCe8oG04450ElhZvNXldgYq','Майки','Пинк',0,'2018-09-06 08:35:32','gally@test.gyg'),(19,'brabos56','$2y$13$otgRD2ySBpx3usUyNpkoMepbHnKONdiI/pvxge1jD9fRr2R0ikfwu','Didier','Trochinski',2,'2018-09-06 08:44:21','sally@test.common'),(21,'danny-woo 76','$2y$13$L2gnWSFACSJYz1M9Fi9VEuBzB0pct2qhy3U.XQg8Bj8D9los0oUYa','Holly','Williams',1,'2018-09-06 10:00:42','gypol@ukr.nest'),(23,'figilly','$2y$13$.pb8/G9d6RtuFRsY/akTL.f7FtEZT2/1WSOvv.r6c8hagLyKSKbmG','Morris','Nillion',0,'2018-09-06 10:03:29','follow76@test.test'),(24,'Marion988','$2y$13$aUxnmEPl7RKb1csF7E26geX3vDWbLjqqSIbbDkjJ2.WvKa2ESTDP.','Иван','Самураев',2,'2018-09-06 10:06:36','gillomano@ukr.netty'),(25,'Вася','$2y$13$8IsFhwx89w0LXoVBT0EPmuoR8hX0YOBzaC11peQX.6rh8cai8xoLK','Аллан','По',1,'2018-09-07 03:41:08','fallan@test.test'),(26,'Кузя','$2y$13$Pf680qTHflwYQoDfnptEkuizwXgrQdm4Y3pvGCFldU5WejFo/6as6','Klyde','White',0,'2018-09-07 03:47:30','fulmino99@gilli.top'),(27,'Брабус Толлис','$2y$13$LBK9/PZEy1VwGTjp4A.ibOKU9YMKdYIbtOocrDFmrzImO9TCB2UpW','Bonny','Johns',2,'2018-09-07 04:04:26','fyllyni@abc.gogo'),(28,'Hugo','$2y$13$qTmWES0fIVfZxVm0LhtgrOWxF0xjjBeTlnPr0OTIYf79Lx78J4fu2','Marrion','Murray',1,'2018-09-07 04:06:21','folitano@gyg.bollo'),(29,'Mannarino76','$2y$13$T5wumJHO0Xr2qRfwv8o2r.Lj3.SdwkHgZoXwnFANMNiFWJGTrmE.S','Don','Salinski',0,'2018-09-07 04:12:21','falloww145@bin.pol');
+INSERT INTO `user` VALUES (1,'Mikky-Donk123','$2y$13$PPBS.DIb84.1cQ1pthpQ9Om6Bb3DkXEYZQ7IjwUuR0NCnEYi5XKja','Mikelangello','Browns',2,'2018-09-05 01:56:37','mikes@test.com'),(2,'lordon','$2y$13$ON2aTHZEa78gJd4PnEbQTO32Iq4C8IqV.a2MrQ9XJZJwNPm0MQtHy','Джонни','Блэк',1,'2018-09-05 19:34:28','john@test.com'),(18,'Monk Donk','$2y$13$WvrPM0fHpmsJ4EyFRsLleOtcWnnkpYwCe8oG04450ElhZvNXldgYq','Майки','Пинк',0,'2018-09-06 08:35:32','gally@test.gyg'),(19,'brabos56','$2y$13$otgRD2ySBpx3usUyNpkoMepbHnKONdiI/pvxge1jD9fRr2R0ikfwu','Didier','Trochinski',2,'2018-09-06 08:44:21','sally@test.common'),(21,'danny-woo 76','$2y$13$L2gnWSFACSJYz1M9Fi9VEuBzB0pct2qhy3U.XQg8Bj8D9los0oUYa','Holly','Williams',1,'2018-09-06 10:00:42','gypol@ukr.nest'),(23,'figilly','$2y$13$.pb8/G9d6RtuFRsY/akTL.f7FtEZT2/1WSOvv.r6c8hagLyKSKbmG','Morris','Nillion',0,'2018-09-06 10:03:29','follow76@test.test'),(24,'Marion988','$2y$13$aUxnmEPl7RKb1csF7E26geX3vDWbLjqqSIbbDkjJ2.WvKa2ESTDP.','Иван','Самураев',2,'2018-09-06 10:06:36','gillomano@ukr.netty'),(25,'Вася','$2y$13$8IsFhwx89w0LXoVBT0EPmuoR8hX0YOBzaC11peQX.6rh8cai8xoLK','Аллан','По',1,'2018-09-07 03:41:08','fallan@test.test'),(26,'Кузя','$2y$13$Pf680qTHflwYQoDfnptEkuizwXgrQdm4Y3pvGCFldU5WejFo/6as6','Klyde','White',0,'2018-09-07 03:47:30','fulmino99@gilli.top'),(27,'Брабус Толлис','$2y$13$LBK9/PZEy1VwGTjp4A.ibOKU9YMKdYIbtOocrDFmrzImO9TCB2UpW','Bonny','Johns',2,'2018-09-07 04:04:26','fyllyni@abc.gogo'),(28,'Hugo','$2y$13$qTmWES0fIVfZxVm0LhtgrOWxF0xjjBeTlnPr0OTIYf79Lx78J4fu2','Marrion','Murray',1,'2018-09-07 04:06:21','folitano@gyg.bollo'),(29,'Mannarino76','$2y$13$T5wumJHO0Xr2qRfwv8o2r.Lj3.SdwkHgZoXwnFANMNiFWJGTrmE.S','Don','Salinski',0,'2018-09-07 04:12:21','falloww145@bin.pol');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -119,7 +119,7 @@ CREATE TABLE `user_to_address` (
   KEY `user_to_address_addresses_id_fk` (`address_id`),
   CONSTRAINT `user_to_address_addresses_id_fk` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user_to_address_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +128,7 @@ CREATE TABLE `user_to_address` (
 
 LOCK TABLES `user_to_address` WRITE;
 /*!40000 ALTER TABLE `user_to_address` DISABLE KEYS */;
-INSERT INTO `user_to_address` VALUES (1,1,1),(2,1,2),(3,2,3),(13,2,2),(16,1,8),(17,1,5),(18,1,7),(24,1,14),(25,18,15),(26,19,16),(27,1,17),(31,23,21),(32,24,22),(33,21,23),(34,1,24),(35,1,25),(39,25,29),(40,26,30),(41,27,31),(42,28,32),(43,29,33),(46,26,25),(47,26,31),(48,29,31),(52,1,33),(55,1,31),(56,19,38),(57,1,39);
+INSERT INTO `user_to_address` VALUES (1,1,1),(2,1,2),(3,2,3),(13,2,2),(16,1,8),(17,1,5),(18,1,7),(24,1,14),(25,18,15),(26,19,16),(27,1,17),(31,23,21),(32,24,22),(33,21,23),(34,1,24),(35,1,25),(39,25,29),(40,26,30),(41,27,31),(42,28,32),(43,29,33),(46,26,25),(47,26,31),(48,29,31),(52,1,33),(55,1,31),(56,19,38),(57,1,39),(62,2,33),(64,2,32);
 /*!40000 ALTER TABLE `user_to_address` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-08 12:09:10
+-- Dump completed on 2018-09-09  2:08:15
